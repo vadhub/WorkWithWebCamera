5c50d74a9995c7128dabff39a2123801ccee59a8
vadim <gabderahmanov99@gmail.com>
2017-08-08 13:01:10 +0400

    test

---
+++ src/com/vad/web/Run.java
@@ -0,0 +1,13 @@
+package com.vad.web;
+
+import JMyron.JMyron;
+
+public class Run {
+
+	public static void main(String[] args) {
+		JMyron m = new JMyron();
+		System.out.println(m.version());
+
+	}
+
+}
