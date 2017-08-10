0d3112f3ac2b553c293fa828a3fd53b583102ecf
vadim <gabderahmanov99@gmail.com>
2017-08-10 13:16:20 +0400

    change

--- src/com/vad/web/Run.java
+++ src/com/vad/web/Run.java
@@ -2,12 +2,15 @@ package com.vad.web;
 
 import JMyron.JMyron;
 
-public class Run {
+public class Run extends JMyron{
 
 	public static void main(String[] args) {
+		setup();
+	}
+	
+	static void setup(){
 		JMyron m = new JMyron();
 		System.out.println(m.version());
-
-	}
+		}
 
 }
